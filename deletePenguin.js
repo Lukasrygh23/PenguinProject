2bdce0b7bfe40a0c4e834d332c34d01c9c16e16c
LukasR23 <Lukas@jazzcatz.com>
2021-11-05 11:15:13 +0000

    :sickos: - delete works.

---
+++ deletePenguin.js
@@ -0,0 +1,47 @@
+"use strict";
+
+//declare everything first
+let idField = document.querySelector("#penguinID");
+let deleteButton = document.querySelector("#killPenguin");
+
+let resultField = document.querySelector("#deleteConfirm");
+
+
+let killPenguin = () => {
+    let num = Number(idField.value);
+    deleteRequest(num);
+};
+
+let deleteRequest = (num) => {
+    let url = "http://localhost:8083/delete/" + num;
+    console.log(url);
+
+    fetch(url, {
+        method: `DELETE`
+    })
+        .then((response) => {
+            if (response.status !== 204) {
+                console.error(`status: ${response.status}`);
+                return;
+            }
+
+            resultField.innerHTML = "You've deleted the penguin at id " + num + ". You monster.";
+
+        });
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//BUTTON
+deleteButton.addEventListener('click', killPenguin);
